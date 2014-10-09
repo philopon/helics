d1385ee1a732dda68a56b095ebc30e7e8c42cbe1
philopon <philopon.dependence@gmail.com>
2014-10-10 03:28:52 +0900

    update example.

--- helics-wai/example.hs
+++ helics-wai/example.hs
@@ -1,27 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad
+import System.Environment
 import Control.Concurrent
+
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Helics
 import Network.Helics.Wai
-import Data.Vault.Lazy as V
+
 import qualified Data.ByteString.Char8 as S8
-import System.Environment
 
 main :: IO ()
 main = do
     key:_ <- getArgs
     withHelics def { licenseKey = S8.pack key, appName = "Test3" } $ do
-        forkIO . void $ sampler 20
-        v <- newKey
-        putStrLn "start"
+        _ <- forkIO $ sampler 20
         run 3000 $ helics app
 
+app :: Application
 app req send = case pathInfo req of
-    []      -> threadDelay (10^5) >> send (responseLBS status200 [] "root")
-    ["foo"] -> threadDelay (2 * 10^5) >> send (responseLBS status200 [] "foo")
-    ["bar"] -> datastoreSegment autoScope (DatastoreSegment "bar" "select" "SELECT * FROM bar WHERE key =  'baz'" "select bars" Nothing) (threadDelay (3 * 10^5) >> send (responseLBS status200 [] "bar")) (transactionId req)
+    []      -> threadDelay (10^(5::Int))     >> send (responseLBS status200 [] "root")
+    ["foo"] -> threadDelay (2 * 10^(5::Int)) >> send (responseLBS status200 [] "foo")
+    ["bar"] -> datastoreSegment autoScope (DatastoreSegment "bar" "select" "SELECT * FROM bar WHERE key =  'baz'" "select bars" Nothing) (threadDelay (3 * 10^(5::Int)) >> send (responseLBS status200 [] "bar")) (transactionId req)
     _       -> send (responseLBS status404 [] "not found")
