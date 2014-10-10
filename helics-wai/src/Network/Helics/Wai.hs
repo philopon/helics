2ed3a68ee0a7e43413980584ebe39465f5faae82
philopon <philopon.dependence@gmail.com>
2014-10-11 04:45:49 +0900

    fix compile error. remove unused dependencies.

--- helics-wai/src/Network/Helics/Wai.hs
+++ helics-wai/src/Network/Helics/Wai.hs
@@ -1,11 +1,12 @@
 module Network.Helics.Wai (helics, transactionId) where
 
 import Network.Wai
+import Network.Helics
 import qualified Network.Helics.Wai.Safe as Safe
 import qualified Data.Vault.Lazy as V
 import System.IO.Unsafe
 
-tidKey :: Key TransactionId
+tidKey :: V.Key TransactionId
 tidKey = unsafePerformIO V.newKey
 {-# NOINLINE tidKey #-}
 
