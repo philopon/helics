b563a6350feccd08fef2151bb65b189fdf0b9e82
philopon <philopon.dependence@gmail.com>
2014-10-11 03:04:56 +0900

    fix travis-install.sh.

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,6 +4,6 @@ BASE=http://download.newrelic.com/agent_sdk
 FILE=`curl $BASE/ | sed -e '/<\/a>/ !d; s/.*<a *href="\(.*\)">.*/\1/g; /tar\.gz/ !d'`
 curl -O $BASE/$FILE
 tar xvf $FILE
-cp -r ${FILE%.tar.gz}/include /usr/local/include
-cp -r ${FILE%.tar.gz}/lib     /usr/local/lib
+sudo cp ${FILE%.tar.gz}/include/* /usr/local/include
+sudo cp ${FILE%.tar.gz}/lib/*     /usr/local/lib
 cabal install --only-dependencies --enable-tests . ./helics-wai
